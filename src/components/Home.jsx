d27c3985bb8d91ea0eb24c5e5bb558380a326901
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2023-11-30 23:10:00 +0530

    Header updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
-import { Image } from '@chakra-ui/image'
-import { Box, Text } from '@chakra-ui/layout'
+import {Box, HStack, Text, Image} from "@chakra-ui/react";
 import React from 'react'
 import bitcoin from "../assests/btc.png"
 import {motion} from "framer-motion";
@@ -7,22 +6,25 @@ import {motion} from "framer-motion";
 const Home = () => {
   return (
     <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
-      <motion.div style={{
-        height:"80vh",
-      }} 
-      animate={{
-        translateY:"20px"
-      }}
-      transition={{
-        duration:2,
-        repeat:Infinity,
-        repeatType:"reverse",
-      }}
-      >
-      <Image w={"full"} h={"76vh"} objectFit={"contain"} src={bitcoin} filter={"grayscale(1)"} />
-      </motion.div>
-      <Text fontSize={"6xl"} textAlign={"center"} fontWeight={"thin"} 
-        color={"whiteAlpha.700"} mt={""}>CryptoX</Text>
+      <HStack w={"full"} display={"flex"} justifyContent={"space-between"} >
+        <Text ml={"12rem"} w={"full"} h={"full"} fontSize={"67px"} color={"whitesmoke"} 
+            textTransform={"uppercase"}>Always<br /> ready<br />
+            to make your<br /> life better</Text>
+        <motion.div style={{
+          height:"80vh",
+        }} 
+        animate={{
+          translateY:"20px"
+        }}
+        transition={{
+          duration:2,
+          repeat:Infinity,
+          repeatType:"reverse",
+        }}
+        >
+        <Image mr={"12rem"} w={"80%"} h={"76vh"} objectFit={"contain"} src={bitcoin} filter={"grayscale(1)"} />
+        </motion.div>
+      </HStack>
     </Box>
   )
 }
