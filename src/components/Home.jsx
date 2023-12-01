425b337ede888932dad881dacf6d083c452b1b3d
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2023-12-01 23:16:17 +0530

    Home page updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,20 @@
-import {Box, HStack, Text, Image} from "@chakra-ui/react";
+import {Box, HStack, Text, Image, VStack} from "@chakra-ui/react";
 import React from 'react'
 import bitcoin from "../assests/btc.png"
 import {motion} from "framer-motion";
 
 const Home = () => {
+  
   return (
-    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
-      <HStack w={"full"} display={"flex"} justifyContent={"space-between"} >
-        <Text ml={"12rem"} w={"full"} h={"full"} fontSize={"67px"} color={"whitesmoke"} 
-            textTransform={"uppercase"}>Always<br /> ready<br />
-            to make your<br /> life better</Text>
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"} color={"white"}>
+      <HStack w={"full"} display={"flex"} justifyContent={"space-evenly"} >
+        <VStack display={"flex"} justifyContent={"center"}  >
+          <Text  w={"full"} h={"full"} fontSize={["20px","20px","30px","45px","60px", "70px"]} 
+              textTransform={"uppercase"} display={"flex"} justifyContent={"center"} >Always<br /> ready<br />
+              to make your<br /> life better</Text>
+          <Text w={"full"} h={"full"} fontSize={["10px","27px"]}
+            display={"flex"} justifyContent={"center"}>Every step of the way</Text>
+        </VStack>
         <motion.div style={{
           height:"80vh",
         }} 
@@ -17,12 +22,13 @@ const Home = () => {
           translateY:"20px"
         }}
         transition={{
-          duration:2,
+          duration:1,
           repeat:Infinity,
           repeatType:"reverse",
         }}
         >
-        <Image mr={"12rem"} w={"80%"} h={"76vh"} objectFit={"contain"} src={bitcoin} filter={"grayscale(1)"} />
+        <Image  w={["40%","60%"]} h={"76vh"} objectFit={"contain"} src={bitcoin} filter={"grayscale(1)"} 
+         m={"auto"} />
         </motion.div>
       </HStack>
     </Box>
